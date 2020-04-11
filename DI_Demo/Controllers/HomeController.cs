693b783129c27c5f4672943541d3cce39a9c7d97
Mio <mio093611@gmail.com>
2020-04-11 18:43:30 +0800

    加入HomeController

---
+++ DI_Demo/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DI_Demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("Random")]
+        public int Random() => new Random().Next();
+    }
+}
